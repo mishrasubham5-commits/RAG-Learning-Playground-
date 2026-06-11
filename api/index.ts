fb93e876f86e27cfd2c1679196f5392096aabd19
mishrasubham5@gmail.com <mishrasubham5@gmail.com>
2026-06-11 15:50:41 +0530

    Fix Vercel function module format

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
-import { app } from "../server";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const { app } = require("../dist/server.cjs");
 
 export default app;
